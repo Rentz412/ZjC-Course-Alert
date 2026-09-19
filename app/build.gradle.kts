9dafbd030c3d227e133c947e30c787c9e1d09c60
Rentz <3215218324@qq.com>
2026-09-20 05:22:35 +0800

    chore: bump version to 1.0.0 (100)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.rentz.zjkb"
         minSdk = 26
         targetSdk = 37
-        versionCode = 10
-        versionName = "0.0.10"
+        versionCode = 100
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
